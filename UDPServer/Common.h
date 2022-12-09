25538db8b9c3707e134ffd45ef2cc8c35ae9129c
M3rl3 <rameezkhatri17.rk@gmail.com>
2022-12-08 21:25:38 -0500

    Added modules to the server.

---
+++ UDPServer/Common.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <WinSock2.h>
+#include <vector>
+
+struct PlayerInfo {
+	float x, z;
+};
+
+struct BulletInfo {
+	float x, z;
+};
+
+struct GameState {
+	std::vector<PlayerInfo[]> players;
+	std::vector<BulletInfo> bullets;
+};
+
+struct ServerInfo {
+	SOCKET socket;
+};
+
+struct ClientInfo {
+	bool HaveInfo;
+	sockaddr_in clientAddr;
+	int clientAddrSize;
+	USHORT playerID;
+};
+
+struct UserInput {
+	bool W, S, A, D;
+};
+
+struct Message {
+	unsigned int messageID;
+};
+
+struct GameStateMessage : public Message {
+
+};
+
+struct UserInputMessage : public Message {
+	bool W;
+	bool S;
+	bool A;
+	bool D;
+};
